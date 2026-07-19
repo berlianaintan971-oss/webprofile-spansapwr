2f02d4cbbb8d793836e0033d317b4051ec005b81
berlianaintan971-oss <berlianaintan971@gmail.com>
2026-07-19 20:11:26 +0700

    feat: menambah favicon logo sekolah pada layout utama

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'SMP Negeri 1 Purworejo')</title>
-
+    
+    <link rel="icon" type="image/png" href="{{ asset('images/logo-sekolah.png') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght=300;400;500;600;700;800&display=swap" rel="stylesheet">
