fc0d7aac25eb348a066a17a4e44a8cf5ddeace44
berlianaintan971-oss <berlianaintan971@gmail.com>
2026-07-19 20:20:06 +0700

    feat: menambah favicon logo sekolah di layout admin dan guest

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Admin SMP NEGERI 1 PURWOREJO</title>
-
+    
+    <link rel="icon" type="image/png" href="{{ asset('images/logo-sekolah.png') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,7 +6,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Portal Akses - SMP Negeri 1 Purworejo</title>
-
+        
+        <link rel="icon" type="image/png" href="{{ asset('images/logo-sekolah.png') }}">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
